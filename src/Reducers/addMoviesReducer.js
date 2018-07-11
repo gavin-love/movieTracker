d18ae9010fb92e48d824ba664b129a0bca433aa4
Keeliana Marler <kmmarler2@hotmail.com>
2018-07-11 10:51:43 -0600

    add addMoviesReducer

---
+++ src/Reducers/addMoviesReducer.js
@@ -0,0 +1,9 @@
+const addMoviesReducer = (state = [], action) => {
+  switch(action.type) {
+    case: 'ADD_MOVIES': 
+      return [...state, action]
+  }
+  default: return state;
+}
+
+export default addMoviesReducer;
