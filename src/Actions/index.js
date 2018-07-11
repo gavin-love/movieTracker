5dec8a503dcc373bcb7bdfbce21c631e97f183ec
Keeliana Marler <kmmarler2@hotmail.com>
2018-07-11 10:46:24 -0600

    Add action creator

---
+++ src/Actions/index.js
@@ -0,0 +1,4 @@
+export const addRecentMovies = (allMovies) => ({
+  type: 'ADD_MOVIES',
+  movies: allMovies
+})
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./container/app/App";
 import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from 'react-redux';
+import { createStore } from 'react-redux';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
