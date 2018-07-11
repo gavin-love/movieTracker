4563f254e7788617da3915fc113420f2952d750d
gavin-love <1co.loco@oprotonmail.ch>
2018-07-11 12:06:53 -0600

    Setup CardContainer functionality and connect mapStateToProps

--- src/container/CardContainer/CardContainer.js
+++ src/container/CardContainer/CardContainer.js
@@ -1,3 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
-// import Card from ''
+import Card from "../../Components/Card/Card";
+
+const CardContainer = ({ movies }) => {
+  console.log(movies);
+};
+
+export const mapStateToProps = state => ({
+  movies: state.movies
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(CardContainer);
