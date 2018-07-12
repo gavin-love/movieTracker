e9ee84d448da76874f3f2025c7631d91f16fe193
Keeliana Marler <kmmarler2@hotmail.com>
2018-07-12 14:32:02 -0600

    Add new user action creator

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,5 +1,13 @@
 export const addRecentMovies = (allMovies) => {
   return {
-  type: 'ADD_MOVIES',
-  movies: allMovies
-}}
+    type: 'ADD_MOVIES',
+    movies: allMovies
+  }
+}
+
+export const updateStoreNewUser = (user) => {
+  return {
+    type: 'ADD_USER',
+    user_id: user.id
+  }
+}
