6ebeb7286e6703b98e3b3c4d8c8203fd8853827e
gavin-love <1co.loco@oprotonmail.ch>
2018-07-11 19:07:08 -0600

    Test mapDispatchToProps for handleMovies

--- src/container/app/App.test.js
+++ src/container/app/App.test.js
@@ -1,9 +1,38 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+import { App, mapDispatchToProps } from "./App";
+import { shallow } from "enzyme";
+import CleanMockData from "../../MockData/MockCleanData";
+
+describe("app", () => {
+  let mockHandleMovies;
+  let wrapper;
+
+  beforeEach(() => {
+    mockHandleMovies = jest.fn();
+    wrapper = shallow(<App handleMovies={mockHandleMovies} />);
+  });
+
+  it.skip("Should call handlesMovies with the correct params", async () => {
+    await wrapper.instance().getMovies();
+
+    expect(mockHandleMovies).toHaveBeenCalledWith(CleanMockData);
+  });
+});
+
+describe("mapDispatch", () => {
+  it("should call dispatch with correct params for handleMovies", () => {
+    const mockDispatch = jest.fn();
+    const mappedProps = mapDispatchToProps(mockDispatch);
+
+    const mockAction = {
+      type: "ADD_MOVIES",
+      movies: CleanMockData
+    };
+
+    mappedProps.handleMovies(CleanMockData);
+
+    expect(mockDispatch).toHaveBeenCalledWith(mockAction);
+  });
+});
