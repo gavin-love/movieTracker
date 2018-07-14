86b84721a2b22d55e66e62eb2784371a51ac504a
gavin-love <1co.loco@oprotonmail.ch>
2018-07-14 10:22:36 -0600

    Add/render Header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Header.css";
+
+export const Header = () => {
+  return <div className="header" />;
+};
--- src/container/app/App.js
+++ src/container/app/App.js
@@ -6,7 +6,7 @@ import { connect } from "react-redux";
 import CardContainer from "../CardContainer/CardContainer";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import Login from "../../Components/Login/Login";
-
+import { Header } from "../../Components/Header/Header";
 
 export class App extends Component {
   componentDidMount() {
@@ -25,18 +25,18 @@ export class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <Header />
         </header>
-        {this.props.error && 
-          <p>{this.props.error}</p>
-        }
+        {this.props.error && <p>{this.props.error}</p>}
         <Switch>
-          <Route exact path="/login" render={() => (
-            this.props.users.user_id ? 
-            <Redirect to='/'/> : 
-            <Login />
-            )}/>
+          <Route
+            exact
+            path="/login"
+            render={() =>
+              this.props.users.user_id ? <Redirect to="/" /> : <Login />
+            }
+          />
         </Switch>
         <Route exact path="/" component={CardContainer} />
       </div>
@@ -48,12 +48,14 @@ export const mapDispatchToProps = dispatch => ({
   handleMovies: movies => dispatch(addRecentMovies(movies))
 });
 
-export const mapStateToProps = (state) => ({
+export const mapStateToProps = state => ({
   users: state.users,
   error: state.error
-})
+});
 
-export default withRouter(connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App)
+);
