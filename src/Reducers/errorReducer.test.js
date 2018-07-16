01fbe32e1e0910e846176ec63a1401efb48acaa7
Keeliana Marler <kmmarler2@hotmail.com>
2018-07-16 14:03:52 -0600

    test errorReducer

--- src/Reducers/errorReducer.test.js
+++ src/Reducers/errorReducer.test.js
@@ -1,1 +1,14 @@
-import 
+import { errorReducer } from './errorReducer.js';
+
+describe('errorReducer', () => {
+  it('returns an error message with type ADD_ERROR', () => {
+    const initialState = '';
+    const mockState = 'hey';
+    const mockAction = {
+      type: 'ADD_ERROR',
+      error: 'hey'
+    }
+    const newState = errorReducer(initialState, mockAction);
+    expect(newState).toEqual(mockState);
+  })
+})
