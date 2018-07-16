286c90549e7f788ca0e1d269745b132d787c3601
Keeliana Marler <kmmarler2@hotmail.com>
2018-07-16 14:54:54 -0600

    added new props to card

--- src/container/Card/Card.test.js
+++ src/container/Card/Card.test.js
@@ -3,20 +3,37 @@ import { shallow } from "enzyme";
 import { Card } from "./Card";
 
 describe("Card", () => {
-  let wrapper;
   let mockProps;
+  let wrapper;
+  let mockHandleSubmitFavorite
+  let mockHandleRemoveFavorite
 
   beforeEach(() => {
     mockProps = {
-      rating: 345,
-      summary: "what?",
-      image: "www.moviesarefoolish.com"
+      vote_average: 345,
+      overview: "what?",
+      poster_path: "www.moviesarefoolish.com",
+      release_date: 56,
+      title: 'movie',
+      movie_id: 4,
+      user: {
+        user_id: 6
+      },
+      favorites: [{}, {}, {}]
     };
 
-    wrapper = shallow(<Card {...mockProps} />);
+    mockHandleSubmitFavorite = jest.fn();
+
+    mockHandleRemoveFavorite = jest.fn();
+
+    wrapper = shallow(<Card {...mockProps} 
+      handleRemoveFavorite={mockHandleRemoveFavorite}
+      handleSubmitFavorite={mockHandleSubmitFavorite}
+      />);
   });
 
   it("should match the snapshot", () => {
     expect(wrapper).toMatchSnapshot();
   });
+
 });
