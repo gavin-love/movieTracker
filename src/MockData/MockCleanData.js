101c236bcc9af5570ca960470d45235ed7e44442
Keeliana Marler <kmmarler2@hotmail.com>
2018-07-17 19:05:53 -0600

    add api tests

--- src/MockData/MockCleanData.js
+++ src/MockData/MockCleanData.js
@@ -1,24 +1,30 @@
 const CleanMockData = [
   {
-    id: 245891,
-    image: "https://image.tmdb.org/t/p/w500/5vHssUeVe25bMrof1HyaPyWgaP.jpg",
-    rating: 7,
-    summary:
-      "Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him."
+    movie_id: 245891,
+    poster_path: "https://image.tmdb.org/t/p/w500/5vHssUeVe25bMrof1HyaPyWgaP.jpg",
+    overview:
+      "Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him.",
+    release_date: "2014-10-22",
+    title: "John Wick",
+    vote_average: 7,
   },
   {
-    id: 245891,
-    image: "https://image.tmdb.org/t/p/w500/5vHssUeVe25bMrof1HyaPyWgaP.jpg",
-    rating: 7,
-    summary:
-      "Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him."
+    movie_id: 245891,
+    poster_path: "https://image.tmdb.org/t/p/w500/5vHssUeVe25bMrof1HyaPyWgaP.jpg",
+    overview:
+      "Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him.",
+    release_date: "2014-10-22",
+    title: "John Wick",
+    vote_average: 7,
   },
   {
-    id: 245891,
-    image: "https://image.tmdb.org/t/p/w500/5vHssUeVe25bMrof1HyaPyWgaP.jpg",
-    rating: 7,
-    summary:
-      "Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him."
+    movie_id: 245891,
+    poster_path: "https://image.tmdb.org/t/p/w500/5vHssUeVe25bMrof1HyaPyWgaP.jpg",
+    overview:
+      "Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him.",
+    release_date: "2014-10-22",
+    title: "John Wick",
+    vote_average: 7,
   }
 ];
 
